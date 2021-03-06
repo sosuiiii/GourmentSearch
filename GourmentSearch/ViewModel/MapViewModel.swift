1fd537c6b7029b867ce51333cdc67e15e32a386c
sosuiiii <wntsssbbsnws@yahoo.co.jp>
2021-03-06 14:53:03 +0900

    検索バリデーション

--- GourmentSearch/ViewModel/MapViewModel.swift
+++ GourmentSearch/ViewModel/MapViewModel.swift
@@ -11,11 +11,12 @@ import RxSwift
 import RxCocoa
 
 protocol MapViewModelInput {
-    
+    var searchText: AnyObserver<String>{get}
 }
 
 protocol MapViewModelOutput {
-    
+    var alert: Observable<AlertType?>{get}
+    var validatedText: Observable<String>{get}
 }
 
 protocol MapViewModelType {
@@ -25,7 +26,31 @@ protocol MapViewModelType {
 
 class MapViewModel: MapViewModelInput, MapViewModelOutput {
     
+    //Input
+    var searchText: AnyObserver<String>
+    //Output
+    var alert: Observable<AlertType?>
+    var validatedText: Observable<String>
+    //property
+    private var disposeBag = DisposeBag()
+    
     init() {
+        let _alert = PublishRelay<AlertType?>()
+        self.alert = _alert.asObservable()
+        
+        let _validatedText = PublishRelay<String>()
+        self.validatedText = _validatedText.asObservable()
+        
+        let _serachText = PublishRelay<String>()
+        self.searchText = AnyObserver<String>() { text in
+            let textOver = TextFieldValidation.validateOverCount(text: text.element!)
+            _serachText.accept(textOver.0)
+            if textOver.1 == nil {return}
+            _alert.accept(textOver.1)
+        }
+        _serachText.subscribe({ text in
+            _validatedText.accept(text.element!)
+        }).disposed(by: disposeBag)
     }
 }
 
