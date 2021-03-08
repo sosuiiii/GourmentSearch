bf6202689cd1dc5922801cacae9eed9a102185cb
sosuiiii <wntsssbbsnws@yahoo.co.jp>
2021-03-08 10:40:03 +0900

    不要な文削除

--- GourmentSearch/Views/DetailSearchView.swift
+++ GourmentSearch/Views/DetailSearchView.swift
@@ -280,17 +280,8 @@ extension DetailSearchView: UISearchBarDelegate {
 
 
 
-//MARK: CollectionViewDelegate,DataSource
-extension DetailSearchView:  UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 15
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GenreCollectionViewCell.reusableIdentifier, for: indexPath) as! GenreCollectionViewCell
-        return cell
-    }
-    
+//MARK: CollectionViewDelegateFlowLayout
+extension DetailSearchView:  UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
